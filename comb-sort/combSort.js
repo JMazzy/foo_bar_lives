a003951b8a2b190c814fc5e2255a031ac78ac75e
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-14 12:15:40 -0400

    almost ready?

--- comb-sort/combSort.js
+++ comb-sort/combSort.js
@@ -1,6 +1,26 @@
-
-
 var combSort = function(array) {
-  // Your code here
-};
+  var gap = array.length;
+  var shrink = 1.3;
+  var swapped = true;
 
+  while ( gap !== 1 || swapped !== false ) {
+    gap = Math.floor( gap / shrink );
+    if ( gap < 1 ) {
+      gap = 1;
+    }
+
+    swapped = false;
+
+    for ( var i = 0; i + gap < array.length; i++ ) {
+      if ( array[i] > array[i+gap] ) {
+        var temp = array[i];
+        array[i] = array[i+gap];
+        array[i+gap] = temp;
+        swapped = true;
+      }
+    }
+  }
+
+  console.log(array);
+  return array;
+};
--- groceries/groceries.js
+++ groceries/groceries.js
@@ -1,2 +1,13 @@
 // Your Code Here!
+$( document ).ready( function() {
 
+  $( "#add-button" ).on("click", function(event) {
+    event.preventDefault();
+    $( "#empty-message" ).remove();
+    var $item = $("#grocery-input").val();
+    $("#grocery-input").val("");
+    console.log($item);
+    $( ".groceries" ).append("<li " + "class='item" + 1 + "'" + ">" + $item + "</li>");
+  });
+
+});
